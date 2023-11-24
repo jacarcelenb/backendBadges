3534c13135f41d3c9e88cbbc2d4842cd1230047d
jacarcelenb <carcelenjorge17@gmail.com>
2023-11-24 17:01:12 -0500

    Modificación esquemas labpack y artifacts

--- modules/labpack/labpack.model.js
+++ modules/labpack/labpack.model.js
@@ -1,57 +1,52 @@
-import mongoose from 'mongoose';
-import model from '../core/model.js';
+import mongoose from "mongoose";
+import model from "../core/model.js";
 
-const LabPackSchema = new mongoose.Schema({
-    package_name:{
-        type: String,
-        required:true
+const LabPackSchema = new mongoose.Schema(
+  {
+    package_name: {
+      type: String,
+      required: true,
     },
-    package_doi:{
-        type: String,
-        required:true
+    package_doi: {
+      type: String,
+      required: true,
     },
     experiment: {
-        type: mongoose.Types.ObjectId,
-        ref: 'experiments',
-        required: true
+      type: mongoose.Types.ObjectId,
+      ref: "experiments",
+      required: true,
     },
-    package_type:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'package_type'
+    package_type: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "package_type",
     },
-    repository:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'repository'
+    repository: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "repository",
     },
-    package_description:{
-        type: String,
-        required:true
-    }
-    ,
-    package_url:{
-        type: String,
-    }
-    ,
-    published:{
-        type: Boolean,
-    }
-    ,
-    submitedZenodo:{
-        type: Boolean,
-    } ,
-    id_zenodo:{
-        type: String,
+    package_description: {
+      type: String,
+      required: true,
     },
-    tokenRepo:{
-        type: String,
-    }
-    ,
-    package_title:{
-        type: String,
-    }
-}, {collection: 'labpack', timestamps: true})
+    package_url: {
+      type: String,
+    },
+    published: {
+      type: Boolean,
+    },
+    submitedZenodo: {
+      type: Boolean,
+    },
+    repoName: {
+      type: String,
+    },
+    owner: {
+      type: String,
+    },
+  },
+  { collection: "labpack", timestamps: true }
+);
 
-const labpacks = mongoose.model('labpack', LabPackSchema);
-
+const labpacks = mongoose.model("labpack", LabPackSchema);
 
 export default model(labpacks);
